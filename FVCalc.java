2238df29674dfa143f35ff0375ed1040428b484e
nirdahan9 <nir.dahan01@post.runi.ac.il>
2024-11-07 12:53:14 +0200

    test 12:53

--- FVCalc.java
+++ FVCalc.java
@@ -2,7 +2,7 @@
 public class FVCalc {
 	public static void main(String[] args){
 		int currentValue = Integer.parseInt(args[0]);
-		double rate = (double) Integer.parseInt(args[1]);
+		double rate = Double.parseDouble(args[1]);
 		double rate2 = ((rate)+100.0)/100;
 		int n = Integer.parseInt(args[2]);
 		int futureValue =(int) (Math.pow(rate2, n)*currentValue);
